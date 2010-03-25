8bb46f6a881755f76c1abb3362d13cdc9575ed72
xzpeter <peter@ubuntu-910.(none)>
2010-03-25 18:27:26 +0800

    adding dm-config.h

---
+++ dm-config.h
@@ -0,0 +1,12 @@
+/*
+ * file		: dm-config.h
+ * desc		: common definitions of iMonitor
+ * mailto	: xzpeter@gmail.com
+ */
+
+#ifndef		__DM_CONFIG_H__
+#define		__DM_CONFIG_H__
+
+#define		MAX_MOBILE_ID_LEN	20
+
+#endif
